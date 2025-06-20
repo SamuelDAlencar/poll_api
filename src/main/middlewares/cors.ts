794f2daf2cebf7235b1e5289a418f37a6315404c
ssup_sam <sarl.2001@hotmail.com>
2025-06-20 17:42:45 -0300

    refactor: add underline to unused param

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-export const cors = (req: Request, res: Response, next: NextFunction): void => {
+export const cors = (_req: Request, res: Response, next: NextFunction): void => {
   res.setHeader("access-control-allow-origin", "*");
   res.setHeader("access-control-allow-methods", "*");
   res.setHeader("access-control-allow-headers", "*");
