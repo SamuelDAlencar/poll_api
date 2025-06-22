13899d052bd902b1f8d811c8036ac9b3a9cf1b5d
ssup_sam <sarl.2001@hotmail.com>
2025-06-22 15:47:22 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -5,32 +5,46 @@ import {
 } from "../../presentation/protocols";
 import { LogControllerDecorator } from "./log";
 
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handler(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: "any_name",
+        },
+      };
+
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+
+  const sut = new LogControllerDecorator(controllerStub);
+
+  return { sut, controllerStub };
+};
+
 describe("LogController Decorator", () => {
   test("Should call controller handle", async () => {
-    class ControllerStub implements Controller {
-      async handler(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: "any_name",
-            email: "any_mail@mail.com",
-            password: "any_password",
-            passwordConfirmation: "any_password",
-          },
-        };
+    const { sut, controllerStub } = makeSut();
 
-        return new Promise((resolve) => resolve(httpResponse));
-      }
-    }
-
-    const controllerStub = new ControllerStub();
     const handleSpy = jest.spyOn(controllerStub, "handler");
-    const sut = new LogControllerDecorator(controllerStub);
 
     const httpRequest = {
       body: {
-        email: "any_mail@mail.com",
         name: "any_name",
+        email: "any_mail@mail.com",
         password: "any_password",
         passwordConfirmation: "any_password",
       },
