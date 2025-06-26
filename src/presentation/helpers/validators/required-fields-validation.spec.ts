f3ac64a273f2321d6008d275a13f3357dc40d2ed
ssup_sam <sarl.2001@hotmail.com>
2025-06-26 00:05:10 -0300

    refactor: move sut to factory method

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParamError } from "../../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
+const makeSut = (field: string): RequiredFieldValidation => {
+  return new RequiredFieldValidation(field);
+};
+
 describe("RequiredFieldValidation", () => {
   test("Should return a MissingParamError if validation fails", () => {
-    const sut = new RequiredFieldValidation("field");
+    const sut = makeSut("field");
 
     sut.validate({ name: "any_name" });
 
@@ -13,7 +17,7 @@ describe("RequiredFieldValidation", () => {
   });
 
   test("Should not return if validation succeeds", () => {
-    const sut = new RequiredFieldValidation("field");
+    const sut = makeSut("field");
 
     const error = sut.validate({ field: "any_value" });
 
