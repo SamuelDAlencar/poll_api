51afecf004bac1349eea4c3e62311b1a7262ac88
ssup_sam <sarl.2001@hotmail.com>
2025-06-28 01:50:28 -0300

    feat: ensure accountmongorepository updates account access token with success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,9 +4,13 @@ import { AddAccountModel } from "../../../../domain/useCases/add-account";
 import { AccountModel } from "../../../../domain/models/account";
 import { MongoHelper } from "../helpers/mongo-helper";
 import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-by-email-repository";
+import { UpdateAccessTokenRepository } from "../../../../data/protocols/criptography/update-access-token-repository";
 
 export class AccountMongoRepository
-  implements AddAccountRepository, LoadAccountByEmailRepository
+  implements
+    AddAccountRepository,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenRepository
 {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection("accounts");
@@ -24,5 +28,19 @@ export class AccountMongoRepository
     });
 
     return account && MongoHelper.map(account);
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    await accountCollection.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          accessToken: token,
+        },
+      }
+    );
   }
 }
