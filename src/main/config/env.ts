aca1eb75129aa721cc5204d69a9795a63cdb248e
ssup_sam <sarl.2001@hotmail.com>
2025-06-29 17:04:21 -0300

    chore: chore

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/poll-api",
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/poll_api",
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || "89Hkls[];"
 };
