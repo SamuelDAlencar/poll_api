efb9521879bd5503a2ef5587bc089a84482c9afa
ssup_sam <sarl.2001@hotmail.com>
2025-06-29 15:21:48 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from "supertest";
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
+import { Collection } from "mongodb";
+import { hash } from "bcrypt";
+
+let accountCollection: Collection;
 
 describe("Login Routes", () => {
   beforeAll(async () => {
@@ -12,9 +16,9 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
 
-    (await accountCollection).deleteMany({});
+    await accountCollection.deleteMany({});
   });
 
   describe("POST /signup", () => {
@@ -26,6 +30,26 @@ describe("Login Routes", () => {
           email: "samuca@email.com",
           password: "123456",
           passwordConfirmation: "123456",
+        })
+        .expect(200);
+    });
+  });
+
+  describe("POST /login", () => {
+    test("Should return 200 on login", async () => {
+      const password = await hash("123456", 12);
+
+      await accountCollection.insertOne({
+        name: "Samuca",
+        email: "samuca@email.com",
+        password,
+      });
+
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "samuca@email.com",
+          password: "123456",
         })
         .expect(200);
     });
