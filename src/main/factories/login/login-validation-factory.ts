e809de63a31a6865234f6fa7321f1bf25bdc1d81
ssup_sam <sarl.2001@hotmail.com>
2025-06-29 13:32:30 -0300

    refactor: move emailvalidatoradapter to main layer

--- src/main/factories/login/login-validation-factory.ts
+++ src/main/factories/login/login-validation-factory.ts
@@ -4,8 +4,7 @@ import {
   EmailValidation,
 } from "../../../presentation/helpers/validators";
 import { Validation } from "../../../presentation/protocols/validation";
-import { EmailValidatorAdapter } from "../../../utils/email-validator-adapter";
-
+import { EmailValidatorAdapter } from "../../adapters/validators/email-validator-adapter";
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
   for (const field of ["email", "password"]) {
