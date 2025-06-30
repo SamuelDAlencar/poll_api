fc5c3eeb4f77d95b474c61e8450b3dc27c4502c5
ssup_sam <sarl.2001@hotmail.com>
2025-06-30 01:13:24 -0300

    feat: add addsurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import app from "../config/app";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
+import { Collection } from "mongodb";
+import { hash } from "bcrypt";
+
+let surveyCollection: Collection;
+
+describe("Survey Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection("surveys");
+
+    await surveyCollection.deleteMany({});
+  });
+
+  describe("POST /surveys", () => {
+    test("Should return 204 add survey success", async () => {
+      await request(app)
+        .post("/api/surveys")
+        .send({
+          question: "Question",
+          answers: [
+            { text: "Answer 1", image: "http://image-url.com/1" },
+            { text: "Answer 2", image: "http://image-url.com/2" },
+          ],
+        })
+        .expect(204);
+    });
+  });
+});
