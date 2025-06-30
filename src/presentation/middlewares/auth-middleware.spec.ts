e9c2d521f22259af9ea43fe15ac80ca529d2ce69
ssup_sam <sarl.2001@hotmail.com>
2025-06-30 01:30:31 -0300

    test: ensure authmiddleware returns 403 if no headers are found

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,14 @@
+import { HttpRequest } from "../protocols/http";
+import { forbidden } from "../helpers/http/http-helper";
+import { AccessDeniedError } from "../errors";
+import { AuthMiddleware } from "./auth-middleware";
+
+describe("Auth Middleware", () => {
+  test("Should return 403 if no x-access-token exists in headers", async () => {
+    const sut = new AuthMiddleware();
+
+    const httpResponse = await sut.handler({});
+
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+});
