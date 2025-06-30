70c9877bd1a6ee54ed9e0b50c2b4a538ab3f702d
ssup_sam <sarl.2001@hotmail.com>
2025-06-29 22:10:20 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,7 +1,6 @@
-import { LogMongoRepository } from "../../../../infra/db/mongodb/log/log-mongo-repository";
 import { LoginController } from "../../../../presentation/controllers/login/login-controller";
 import { Controller } from "../../../../presentation/protocols";
-import { LogControllerDecorator } from "../../../decorators/log-controller-decorator";
+import { makeLogControllerDecorator } from "../../decorators/log-controller-decorator-factory";
 import { makeDbAuthentication } from "../../usecases/authentication/db-authentication-factory";
 import { makeLoginValidation } from "./login-validation-factory";
 
@@ -11,7 +10,5 @@ export const makeLoginController = (): Controller => {
     makeLoginValidation()
   );
 
-  const logMongoRepository = new LogMongoRepository();
-
-  return new LogControllerDecorator(loginController, logMongoRepository);
+  return makeLogControllerDecorator(loginController);
 };
--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,27 +1,16 @@
 import { SignUpController } from "../../../../presentation/controllers/signup/signup-controller";
-import { DbAddAccount } from "../../../../data/useCases/add-account/db-add-account";
-import { BcryptAdapter } from "../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter";
-import { AccountMongoRepository } from "../../../../infra/db/mongodb/account/account-mongo-repository";
-import { LogMongoRepository } from "../../../../infra/db/mongodb/log/log-mongo-repository";
-import { LogControllerDecorator } from "../../../decorators/log-controller-decorator";
 import { Controller } from "../../../../presentation/protocols";
 import { makeSignUpValidation } from "./signup-validation-factory";
 import { makeDbAuthentication } from "../../usecases/authentication/db-authentication-factory";
+import { makeDbAddAccount } from "../../usecases/add-account/db-add-account-factory";
+import { makeLogControllerDecorator } from "../../decorators/log-controller-decorator-factory";
 
 export const makeSignUpController = (): Controller => {
-  const salt = 12;
-
-  const bcryptAdapter = new BcryptAdapter(salt);
-  const accountMongoRepository = new AccountMongoRepository();
-
-  const addAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
-
   const signUpController = new SignUpController(
-    addAccount,
+    makeDbAddAccount(),
     makeSignUpValidation(),
     makeDbAuthentication()
   );
-  const logMongoRepository = new LogMongoRepository();
 
-  return new LogControllerDecorator(signUpController, logMongoRepository);
+  return makeLogControllerDecorator(signUpController);
 };
