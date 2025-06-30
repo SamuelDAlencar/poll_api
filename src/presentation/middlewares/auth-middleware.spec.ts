b6af4c40baf827c9a24c38f03c46acf8735d9eb2
ssup_sam <sarl.2001@hotmail.com>
2025-06-30 01:40:14 -0300

    test: ensure auth middleware calls load account with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,13 +2,55 @@ import { HttpRequest } from "../protocols/http";
 import { forbidden } from "../helpers/http/http-helper";
 import { AccessDeniedError } from "../errors";
 import { AuthMiddleware } from "./auth-middleware";
+import { LoadAccountByToken } from "../../domain/useCases/load-account-by-token";
+import { AccountModel } from "../../domain/models/account";
+
+const makeFakeAccount = (): AccountModel => ({
+  id: "any_id",
+  name: "any_name",
+  email: "any_email",
+  password: "any_password",
+});
 
 describe("Auth Middleware", () => {
   test("Should return 403 if no x-access-token exists in headers", async () => {
-    const sut = new AuthMiddleware();
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load(
+        accessToken: string,
+        role?: string
+      ): Promise<AccountModel | null> {
+        return new Promise((resolve) => resolve(makeFakeAccount()));
+      }
+    }
+
+    const loadAccontByTokenStub = new LoadAccountByTokenStub();
+
+    const sut = new AuthMiddleware(loadAccontByTokenStub);
 
     const httpResponse = await sut.handler({});
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  test("Should call LoadAccountByToken with correct accessToken", async () => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load(
+        accessToken: string,
+        role?: string
+      ): Promise<AccountModel | null> {
+        return new Promise((resolve) => resolve(makeFakeAccount()));
+      }
+    }
+
+    const loadAccontByTokenStub = new LoadAccountByTokenStub();
+    const loadSpy = jest.spyOn(loadAccontByTokenStub, "load");
+
+    const sut = new AuthMiddleware(loadAccontByTokenStub);
+
+    await sut.handler({
+      headers: { "x-access-token": "any_token" },
+    });
+
+    expect(loadSpy).toHaveBeenCalledWith("any_token");
   });
 });
