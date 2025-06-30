c3add0234bc286d15dade9d3399520b97122feba
ssup_sam <sarl.2001@hotmail.com>
2025-06-30 01:40:09 -0300

    feat: ensure auth middleware calls load account with correct token

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -2,11 +2,18 @@ import { AccessDeniedError } from "../errors";
 import { forbidden } from "../helpers/http/http-helper";
 
 export class AuthMiddleware {
-  constructor() {}
+  constructor(
+    private readonly loadAccountByToken: any = null,
+    private readonly role?: string
+  ) {}
 
   async handler(httpRequest: any): Promise<any> {
-    const error = forbidden(new AccessDeniedError());
+    const accessToken = httpRequest.headers?.["x-access-token"];
 
-    return new Promise((resolve) => resolve(error));
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken);
+    }
+
+    return forbidden(new AccessDeniedError());
   }
 }
