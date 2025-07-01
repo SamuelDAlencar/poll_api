9b66fcb05c20745d035e0fe1b58913e8bc8313ea
ssup_sam <sarl.2001@hotmail.com>
2025-07-01 00:19:16 -0300

    test: ensure add survey rout returns 204 if valida ccess token

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from "supertest";
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import { Collection } from "mongodb";
+import { sign } from "jsonwebtoken";
+import env from "../config/env";
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 describe("Survey Routes", () => {
   beforeAll(async () => {
@@ -16,8 +19,10 @@ describe("Survey Routes", () => {
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection("surveys");
+    accountCollection = await MongoHelper.getCollection("accounts");
 
     await surveyCollection.deleteMany({});
+    await accountCollection.deleteMany({});
   });
 
   describe("POST /surveys", () => {
@@ -32,6 +37,37 @@ describe("Survey Routes", () => {
           ],
         })
         .expect(403);
+    });
+
+    test("Should return 204 on add survey with valid accessToken", async () => {
+      const res = await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+        role: "admin",
+      });
+
+      const id = res.ops[0]._id;
+      const accessToken = sign({ id }, env.jwtSecret);
+
+      await accountCollection.updateOne(
+        { _id: id },
+        {
+          $set: { accessToken },
+        }
+      );
+
+      await request(app)
+        .post("/api/surveys")
+        .set("x-access-token", accessToken)
+        .send({
+          question: "Question",
+          answers: [
+            { text: "Answer 1", image: "http://image-url.com/1" },
+            { text: "Answer 2", image: "http://image-url.com/2" },
+          ],
+        })
+        .expect(204);
     });
   });
 });
