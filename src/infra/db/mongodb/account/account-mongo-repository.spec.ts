9380bf5bdea39cbcc6faf6783abd77adf4302c72
ssup_sam <sarl.2001@hotmail.com>
2025-06-30 23:52:11 -0300

    test: ensure accountmongorepository finds an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -66,29 +66,58 @@ describe("Account Mongo Repository", () => {
 
       expect(account).toBeFalsy();
     });
+  });
 
-    describe("updateAccessToken()", () => {
-      test("Should update the account access token on updateAccessToken success", async () => {
-        const sut = makeSut();
+  describe("updateAccessToken()", () => {
+    test("Should update the account access token on updateAccessToken success", async () => {
+      const sut = makeSut();
 
-        const res = await accountCollection.insertOne({
-          name: "any_name",
-          email: "any_email@mail.com",
-          password: "any_password",
-        });
-
-        const id = res.ops[0]._id;
-
-        expect(res.ops[0].accessToken).toBeFalsy();
-
-        await sut.updateAccessToken(id, "any_token");
-        const account = await accountCollection.findOne({
-          _id: id,
-        });
-
-        expect(account).toBeTruthy();
-        expect(account.accessToken).toBe("any_token");
+      const res = await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
       });
+
+      const id = res.ops[0]._id;
+
+      expect(res.ops[0].accessToken).toBeFalsy();
+
+      await sut.updateAccessToken(id, "any_token");
+      const account = await accountCollection.findOne({
+        _id: id,
+      });
+
+      expect(account).toBeTruthy();
+      expect(account.accessToken).toBe("any_token");
+    });
+  });
+
+  describe("loadByToken()", () => {
+    test("Should return an account on loadByToken success", async () => {
+      const sut = makeSut();
+
+      await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        accessToken: 'any_token',
+      });
+
+      const account = await sut.loadByToken("any_token");
+
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe("any_name");
+      expect(account.email).toBe("any_email@mail.com");
+      expect(account.password).toBe("any_password");
+    });
+
+    test("Should return null if loadByEmail fails", async () => {
+      const sut = makeSut();
+
+      const account = await sut.loadByEmail("any_email@mail.com");
+
+      expect(account).toBeFalsy();
     });
   });
 });
